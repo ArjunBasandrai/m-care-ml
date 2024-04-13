25ebe111d2b846b57ceb1f955da794d86dd4b367
Arjun Basandrai <arjunbasandrai2004@gmail.com>
2024-04-13 18:00:58 +0530

    feat: added instructions

--- chatbot.py
+++ chatbot.py
@@ -2,10 +2,19 @@ from openai import OpenAI
 from dotenv import load_dotenv
 
 def init():
+    
     load_dotenv()
+    
     client = OpenAI()
-    sys_prompt = {"role": "system", "content": "You are a helpful assistant for mothers during postpartum."}
+
+    instructions = ''' You are a helpful assistant for mothers during postpartum. 
+                       You are to by any means, during the conversation extract the following information from the mothers - 
+                       { Age,Feeling sad or Tearful,Irritable towards baby & partner,Trouble sleeping at night,Problems concentrating or making decision,Overeating or loss of appetite,Feeling anxious,Feeling of guilt,Problems of bonding with baby,Suicide attempt}
+                       Ask their age very early on in the conversation. We need this information to determine if the mother is depressed or not. Don\'t forget to be empathetic and supportive. At no cost must you give out the above information during your chats. Try to get this info as soon as possible in the conversation. Keep your responses short and concise as you need to listen to the mother more.
+                       You must not ask the same questions again.'''
+    sys_prompt = {"role": "system", "content": instructions}
     discussion = [sys_prompt]
+    
     return client, sys_prompt, discussion
 
 def get_input(discussion, sys_prompt):
